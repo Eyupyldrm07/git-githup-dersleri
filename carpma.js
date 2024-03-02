300d5d015612e724160e230e5cd2664e07199a0d
mr.alderson <mradlerson07@hotmail.com>
2024-03-02 16:13:39 +0300

    hocama gönderdigim ilk projem

---
+++ carpma.js
@@ -0,0 +1,9 @@
+const sayi1 = document.getElementById('sayi1');
+const sayi2 = document.getElementById('sayi2');
+
+const sonuc = document.getElementById('sonuc');
+
+fonction carp () {
+sonuc.innerHTML = sayi1.value * sayi2.value;
+
+}
